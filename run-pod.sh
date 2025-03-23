3ec9b5cb62727777b954ad9ccfe963cbebee01b5
Chad Phillips <chad@apartmentlines.com>
2025-03-23 18:35:48 -0400

    adjust run pod for 1 hour idle, install SSH keys

--- run-pod.sh
+++ run-pod.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-limit=${1}
-processing_limit=${2}
+# limit=${1}
+# processing_limit=${2}
+ssh_dir="${HOME}/.ssh"
 
-transcription-processor --limit "${limit}" --processing-limit "${processing_limit}"
+mkdir -p "${ssh_dir}" && \
+  chmod 700 "${ssh_dir}" && \
+  echo "${PUBLIC_KEY}" > "${ssh_dir}/authorized_keys" && \
+  chmod 600 "${ssh_dir}/authorized_keys"
+
+# transcription-processor --limit "${limit}" --processing-limit "${processing_limit}"
+sleep 1h
 runpodctl stop pod "${RUNPOD_POD_ID}"
