76d56fe6f56f09a7f7f86ca4da7ef397fac507fb
Chad Phillips <chad@apartmentlines.com>
2025-03-23 20:19:58 -0400

    start ssh

--- run-pod.sh
+++ run-pod.sh
@@ -7,7 +7,8 @@ ssh_dir="${HOME}/.ssh"
 mkdir -p "${ssh_dir}" && \
   chmod 700 "${ssh_dir}" && \
   echo "${PUBLIC_KEY}" > "${ssh_dir}/authorized_keys" && \
-  chmod 600 "${ssh_dir}/authorized_keys"
+  chmod 600 "${ssh_dir}/authorized_keys" && \
+  service ssh start
 
 # transcription-processor --limit "${limit}" --processing-limit "${processing_limit}"
 sleep 1h
