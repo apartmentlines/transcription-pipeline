9252e0d0e08d1f3332e5aa85820f8381b3e52d6a
Chad Phillips <chad@apartmentlines.com>
2025-04-03 13:25:52 -0400

    conditional execution and done output

--- dockerhub-build.sh
+++ dockerhub-build.sh
@@ -8,10 +8,11 @@ DOCKERHUB_PROJECT_TAG="v0.1.0"
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 echo "Building docker image ${LOCAL_DOCKER_IMAGE}"
-docker compose build
-echo "Running setup script on ${LOCAL_DOCKER_IMAGE}"
-"${SCRIPT_DIR}/setup.sh"
-echo "Committing ${LOCAL_DOCKER_IMAGE} changes to ${DOCKERHUB_USERNAME}/${DOCKERHUB_PROJECT}:${DOCKERHUB_PROJECT_TAG}"
-docker commit ${LOCAL_DOCKER_IMAGE} ${DOCKERHUB_USERNAME}/${DOCKERHUB_PROJECT}:${DOCKERHUB_PROJECT_TAG}
-echo "Pushing changes to ${DOCKERHUB_USERNAME}/${DOCKERHUB_PROJECT}:${DOCKERHUB_PROJECT_TAG}"
-docker push ${DOCKERHUB_USERNAME}/${DOCKERHUB_PROJECT}:${DOCKERHUB_PROJECT_TAG}
+docker compose build && \
+echo "Running setup script on ${LOCAL_DOCKER_IMAGE}" && \
+"${SCRIPT_DIR}/setup.sh" && \
+echo "Committing ${LOCAL_DOCKER_IMAGE} changes to ${DOCKERHUB_USERNAME}/${DOCKERHUB_PROJECT}:${DOCKERHUB_PROJECT_TAG}" && \
+docker commit ${LOCAL_DOCKER_IMAGE} ${DOCKERHUB_USERNAME}/${DOCKERHUB_PROJECT}:${DOCKERHUB_PROJECT_TAG} && \
+echo "Pushing changes to ${DOCKERHUB_USERNAME}/${DOCKERHUB_PROJECT}:${DOCKERHUB_PROJECT_TAG}" && \
+docker push ${DOCKERHUB_USERNAME}/${DOCKERHUB_PROJECT}:${DOCKERHUB_PROJECT_TAG} && \
+echo "DONE!"
