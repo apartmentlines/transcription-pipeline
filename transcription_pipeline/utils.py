29cf724e3b0c76d46fc893ef69e19b21543fb076
Chad Phillips <chad@apartmentlines.com>
2025-04-08 21:45:01 -0400

    fix type hint

--- transcription_pipeline/utils.py
+++ transcription_pipeline/utils.py
@@ -43,7 +43,7 @@ def get_request(url: str, params: dict) -> requests.Response:
     stop=stop_after_attempt(DEFAULT_RETRY_ATTEMPTS),
     wait=wait_exponential(multiplier=DEFAULT_RETRY_BACKOFF),
 )
-def post_request(url: str, data: dict[str, str], json: bool = False) -> requests.Response:
+def post_request(url: str, data: dict[str, Any], json: bool = False) -> requests.Response:
     kwargs: dict[str, Any] = {
         "timeout": DOWNLOAD_TIMEOUT,
     }
